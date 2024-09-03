091260480676d382f8becf55e1361244b710ff00
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2024-09-03 18:45:07 -0300

    AULA02
    TESTE

--- aula02/Program.cs
+++ aula02/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
-
+﻿// usando biblioteca do sistema
+using System;
+//namespace pra organizar
 namespace Aula02
-{
+{//classe
     class Program
-    {
+    {//criando metodo menu
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Word!!");
-            
+        {   //exibir frase
+            Console.WriteLine("Fala zé");
+            //se maior que 0 - exiba um resultado
             if (args.GetLength(0) > 0)
             {
                 Console.WriteLine(args.GetValue(0));
-            }
+            } //senão esta funcionando sem valor
             else
             {
                 Console.Write("Está funcionando");
