94cfa384a4f56a34898cbdac06db9cfe21a740be
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2024-09-05 20:09:21 -0300

    AULA04 - ESCOPO DE VARIAVEIS

---
+++ Aula04/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+//criando classe
+class Aula04{
+    //criando variavel global
+    static int num=10;
+    //metodo principal
+    static void Main(){
+        Console.WriteLine("O valor da variavel num , global é : "+num);
+    }
+}
