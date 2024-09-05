a5c3bda8dd85317c11dcc2074db5b87bf3ef455d
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2024-09-05 20:00:07 -0300

    AULA 03 - VARIAVEIS

---
+++ Aula03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Aula03{
+
+    static void Main(){
+        //criando variaveis
+        byte n1=10;
+        int num=-10;
+        char letra='8';
+        float valor=5.3f;
+        string nome="Bruno";
+        //setando valor a variavel aux
+        var aux=nome;
+        //exibindo o nome do funcionário
+        Console.WriteLine("O nome do funcionário é: "+aux);
+    }
+}
--- Aula03/obj/Debug/net8.0/Aula03.AssemblyInfo.cs
+++ Aula03/obj/Debug/net8.0/Aula03.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("aula02")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Aula03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a5bc6ff7c5571d629d06d0d34e71ba7d2d6735")]
-[assembly: System.Reflection.AssemblyProductAttribute("aula02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("aula02")]
+[assembly: System.Reflection.AssemblyProductAttribute("Aula03")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Aula03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
